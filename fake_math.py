b81fde5fc469b37b9bd0be6b59e9f2c6705b3bf2
Kyzm7 <kyzm7@mail.ru>
2024-09-17 12:10:02 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide (first, second):
+    if second == 0:
+        rez = 'Ошибка'
+        return rez
+    rez1 = first/second
+    return rez1
---
+++ true_math.py
@@ -0,0 +1,6 @@
+def divide (first, second):
+    if second == 0:
+        from math import inf
+        return inf
+    rez1 = first/second
+    return rez1
